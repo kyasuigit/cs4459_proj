d169c20a1e531a9b838a3157b89f61dc438dbbf1
Kevin Yang <kev4238@gmail.com>
2024-04-09 16:57:33 -0400

    Added bugs to the code (April 9, 2024)

--- chord_consistent_hashing.py
+++ chord_consistent_hashing.py
@@ -56,11 +56,44 @@ class ChordConsistentHash:
                 break
 
         return successor
+    
+    def find_predecessor(self, key):
+        predecessor = None
+
+        for i in range (key - 1, key - len(self.nodes), -1):
+            if self.nodes[i % (2 ** self.num_nodes)]:
+                predecessor = i % (2 ** self.num_nodes)
+                break
+        
+        return predecessor
+
+    
+    # method for adding a new node to the hash
+    # need to find the predecessor and get everything stored at the node in the finger table 
+    # and create a new finger table in the currently added node to store all of the items
+    def redistribute_items(self, new_node_id):
+        new_node = self.nodes[new_node_id]
+
+        predecessor_id = self.find_predecessor(new_node_id)
+
+        if predecessor_id is not None:
+            predecessor_node = self.nodes[predecessor_id]
+
+            for finger_start, finger_sucessor in predecessor_node.finger_table:
+                if finger_sucessor == new_node_id:
+                    items_to_transfer = self.redis_conn.keys(f"Chord_{finger_start}_*")
+                    for item_key in items_to_transfer:
+                        item_value = self.redis_conn.get(item_key)
+                        new_key = item_key.replace(str(finger_sucessor), str(new_node_id))
+                        self.redis_conn.set(new_key, item_value)
+
+                    self.redis_conn.delete(*items_to_transfer)
+
 
     def add_item(self, item_id, data):
         hash_key = self.obtain_hash(item_id)
         succesor_node = self.find_successor(hash_key)
-        self.redis_conn.set(f"Chord_{succesor_node}_{hash_key}", data[1])
+        self.redis_conn.set(f"Chord_{succesor_node}_{hash_key}", f"{data[0]},{data[1]}")
 
     def get_item(self, item_id):
         hashed_id = self.obtain_hash(item_id)
@@ -74,6 +107,14 @@ class ChordConsistentHash:
 
         node_containing_item = find_item(self.start_point)
         return self.redis_conn.get(f"Chord_{node_containing_item}_{hashed_id}")
+    
+    def get_all_items(self, node_id):
+        listOfItems = []
+        for key in self.redis_conn.scan_iter(f"Chord_{node_id}_*"):
+            listOfItems.append(self.redis_conn.get(key))
+        
+        return listOfItems
+
 
 if __name__ == "__main__":
     chords = ChordConsistentHash(5)
@@ -82,5 +123,3 @@ if __name__ == "__main__":
     for x in chords.nodes:
         if x:
             print(x.node_id, x.items)
-
-
--- inventory.py
+++ inventory.py
@@ -6,13 +6,22 @@ class InventorySystem:
     def __init__(self, node_num):
         self.redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
         self.chord = ChordConsistentHash(node_num, self.redis_conn)
+        self.num_nodes = node_num
 
     def add_item(self, item_name, quantity):
         self.chord.add_item(item_name, [item_name, quantity])
 
     def get_item_quantity(self, item_name):
         node = self.chord.get_item(item_name)
-
         node_as_string = str(node.decode('utf-8')) if node else "Not Found"
-
         return node_as_string
+    
+    def get_number_of_nodes(self):
+        return self.num_nodes
+    
+    def change_chord_algorithm (self, new_node):
+        self.chord = ChordConsistentHash(new_node, self.redis_conn)
+        self.num_nodes = new_node
+
+    def get_all_items(self, node_id):
+        return self.chord.get_all_items(node_id)
